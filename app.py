0cdb134781be391e15f50abc1ad1a2e64d2423e9
Thiago Terleski <thiagoterleski@Mac-mini-de-Thiago.local>
2017-09-24 16:52:28 -0300

    configuring websocket

--- app.py
+++ app.py
@@ -1,32 +1,68 @@
-#!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, g, abort, jsonify, make_response
+from pprint import pprint
+import time, uuid, sqlite3
+from aiohttp import web
+from flask_socketio import SocketIO, join_room, leave_room
 
-import time
+DATABASE = 'database.db'
 
 app = Flask(__name__)
 
-games = [
-    {
-        'id': 1,
-        'hash': 'd41d8cd98f00b204e9800998ecf8427e',
-        'created': time.strftime("%I:%M:%S")
-    },
-    {
-        'id': 2,
-        'hash': '07cc694b9b3fc636710fa08b6922c42b',
-        'created': time.strftime("%I:%M:%S")
-    },
-]
+socketio = SocketIO(app)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    return (rv[0] if rv else None) if one else rv
 
 @app.route('/')
 
 def index():
-    return "Hello, World!"
+    return "Start app"
 
-@app.route('/games', methods=['GET'])
+# List games
+@app.route('/game', methods=['GET'])
+def get_games():
+    cur = get_db().execute('select * from game')
+    return jsonify({ 'results': cur.fetchall()})
 
-def getGames():
-    return jsonify({'games': games})
+# Get game by id
+@app.route('/game/<string:game_id>', methods=['GET'])
+def get_game(game_id):
+    cur = get_db().execute("select * from game WHERE id = ?", [game_id])
+    game = cur.fetchone()
+    return jsonify({ 'game': game })
+
+# Create a new Game
+@app.route('/game', methods=['POST'])
+def create_game():
+    game_hash = str(uuid.uuid1())
+    cur = get_db().execute("insert into game (id) values (?)", [game_hash])
+    get_db().commit()
+
+    return jsonify({ 'result': game_hash })
+
+# Socket functions
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
+
+
